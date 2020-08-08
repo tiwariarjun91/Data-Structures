ab522b89edc4ff26fdd5e8374635e7051dc4b1f1
tiwariarjun91 <tiwariarjun91@gmail.com>
2020-08-08 22:05:18 +0530

    UnionFind_using_array_DS

---
+++ UnionFind.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class UnionFind {
+    private int id[];
+    public UnionFind(int n){
+        id = new int[n];
+        for(int i = 0; i<n ; i++){
+             id[i] = i;
+        }
+    }
+
+    public boolean find(int a, int b){
+        return id[a]==id[b];
+    }
+
+    public void union(int a, int b, int n){
+        int a1 = a;
+        int a2 = b;
+        if(id[a]!=id[b]){
+            for(int i = 0; i< n ;i++){
+
+                if(id[i]==id[a1]){
+                    id[i] = a2;
+                }
+            }
+
+        }
+
+
+    }
+        
+
+    
+    public static void main(String[] args) {
+        Scanner ss = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int n = ss.nextInt();
+        UnionFind uf = new UnionFind(n);
+        for(int i=0; i<n ; i++)
+        {
+        System.out.println(uf.id[i]);
+        }
+        System.out.println(uf.find(2,3));
+        uf.union(2,3, n);
+        uf.union(3,4, n);
+        System.out.println(uf.find(2,4));
+        for(int i=0; i<n ; i++)
+        {
+        System.out.print(uf.id[i]);
+        }
+
+        
+       /* while(ss.hasNextInt())
+        {
+            int m = ss.nextInt();
+            int p = ss.nextInt();
+
+        
+        System.out.println(n);
+        System.out.println(m);
+        System.out.println(p);
+        }*/
+
+    }
+    
+}
