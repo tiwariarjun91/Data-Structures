2369763642b7c0b7ef83f226a3a2aa5876013825
tiwariarjun91 <tiwariarjun91@gmail.com>
2021-10-13 23:58:30 +0530

    Lets do this

---
+++ Python/Searching/Unordered_Search.py
@@ -0,0 +1,10 @@
+def UnorderedSearch(nums,target)-> bool:
+    for i in range(len(nums)):
+        if target == nums[i]:
+            return True
+
+    return False
+
+nums = [1,84,87,0,45,36,700,89,46,2,9,14,65]
+print(UnorderedSearch(nums,2))
+print(UnorderedSearch(nums,100))
